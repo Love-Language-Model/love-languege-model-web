18f07c2a1b6d88e3088b3b0774f7f7a9a48afb7e
Guilherme Moura <glhrmoura@gmail.com>
2025-08-15 10:04:05 -0300

    refactor: simplify AppLayout by removing local auth state

--- src/layouts/app.tsx
+++ src/layouts/app.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Header from '@/components/Header';
 
 const AppLayout = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(true);
-
   return (
     <div className="min-h-screen flex flex-col bg-white">
-      <Header whiteBackground isAuthenticated={isAuthenticated} />
+      <Header whiteBackground />
       <Outlet />
     </div>
   );
