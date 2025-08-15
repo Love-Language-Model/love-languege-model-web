17a1a897c9e93d00bee56a934478c927c2896910
Guilherme Moura <glhrmoura@gmail.com>
2025-08-15 09:14:40 -0300

    feat: replace BrowserRouter with RouterProvider

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import Chat from '@/pages/Chat';
-import Index from '@/pages/Index';
-import Login from '@/pages/Login';
-import SignUp from '@/pages/SignUp';
-import Profile from '@/pages/Profile';
+import { router } from '@/routes';
 
 import { Toaster } from '@/components/ui/toaster';
 import { TooltipProvider } from '@/components/ui/tooltip';
@@ -18,15 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/profile/*" element={<Profile />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </TooltipProvider>
   </QueryClientProvider>
 );
