329548d10f80bf5f7950d1e30acb8d332d7df91b
Guilherme Moura <glhrmoura@gmail.com>
2025-08-15 16:33:18 -0300

    refactor: improve Chat page layout with better spacing and container classes

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -43,9 +43,9 @@ const Chat = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col pt-24">
-      <div className="flex-1 flex justify-center py-4 px-4">
-        <div className="w-full max-w-4xl flex flex-col max-h-[calc(100dvh-150px)]">
+    <div className="min-h-screen bg-gray-50 flex flex-col pt-[75px] md:pt-20">
+      <div className="flex-1 flex justify-center mt-4">
+        <div className="w-full container flex flex-col max-h-[calc(100dvh-150px)]">
           {messages.length === 0 ? (
             <EmptyState
               message={message}
