bcb630e07fe98cbe86450071c453d74b0e347e6d
Sergio Rodrigues Pinto <exprodrigues@gmail.com>
2026-01-18 17:32:35 -0300

    chore(vite): update dev server port to 8080

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 export default defineConfig(({ mode }) => ({
   server: {
     host: '::',
-    port: 3000,
+    port: 8080,
   },
   plugins: [react()],
   resolve: {
